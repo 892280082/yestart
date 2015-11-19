77537d025177d50c993e719bb64b1357aeb8ed48
892280082 <892280082@qq.com>
2015-11-19 17:36:23 +0800

    route ok!

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+router.get('/login1', function(req, res, next) {
+  res.render('login1');
+});
+
+router.get('/login3', function(req, res, next) {
+  res.render('login3');
+});
+
+router.get('/login4', function(req, res, next) {
+  res.render('login4');
+});
+
+router.get('/main', function(req, res, next) {
+  res.render('main');
+});
+
+
+
 module.exports = router;
