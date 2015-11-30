36346175d8aa05fd2ea907308e5d5f4eef086eb7
892280082 <892280082@qq.com>
2015-11-30 22:56:02 +0800

    page 160 promise ok

--- routes/angular.js
+++ routes/angular.js
@@ -8,9 +8,43 @@ router.get('/cached', function(req, res, next) {
   res.render('angular/cached');
 });
 
+router.get('/self_cached', function(req, res, next) {
+  res.render('angular/self_cached');
+});
+
 router.get('/data', function(req, res, next) {
 	res.send('今天天气好啊');
 });
+
+/**resource*/
+router.get('/resource_a7_7', function(req, res, next) {
+  res.render('angular/resource_a7_7');
+});
+
+router.get('/data/info', function(req, res, next) {
+	var action = req.query.action;
+		var array = [
+			{name:'aa',score:50,sex:'man'},
+			{name:'aa',score:50,sex:'man'},
+			{name:'aa',score:50,sex:'man'}
+		]
+		res.send(array);
+});
+
+router.post('/data/info', function(req, res, next) {
+	res.send(0);
+});
+
+/**promise*/
+router.get('/promise', function(req, res, next) {
+	res.render('angular/promise');
+});
+
+
+
+
+
+
 
 
 module.exports = router;
