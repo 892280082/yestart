9ec4639261f05325712d4311440d4f82a7098367
892280082 <892280082@qq.com>
2015-12-12 17:03:28 +0800

    rm

--- routes/back.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var Admin = require('../models/admin.js');
-var merges = require('../self_modules/merges/merges.js');
-var router = express.Router();
-
-
-
-module.exports = router;
--- settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	cookieSecret:'yestart',
-	db:'yestart',
-	host:'localhost',
-	port:27017
-}
